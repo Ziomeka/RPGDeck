12601dff4e7e93c236951242a0292de70e3f862c
Agnieszka Ziomek <a.ziomek@nowaera.pl>
2018-02-27 10:46:12 +0100

    Create deck module

--- src/js/app.js
+++ src/js/app.js
@@ -6,67 +6,71 @@ Widok bedzie brał dane karty i produkował odpowiednie fragmenty innerHTML
 Controller bedzie słuchał na buttonach w DOM, wysyłał do modelu "dawaj kartę" po czym przekazywał do widoku, masz du dane i weź się przeładuj.
 */
 (function() {
-  let deck = [{
-      id: 0,
-      title: 'Start',
-      image: 'end.jpg'
-    },
-    {
-      id: 1,
-      title: 'The Paladin',
-      image: 'card01.jpg'
-    },
-    {
-      id: 2,
-      title: 'The Keep',
-      image: 'card02.png'
-    },
-    {
-      id: 3,
-      title: 'The Desert',
-      image: 'card03.jpg'
-    },
-    {
-      id: 4,
-      title: 'The Wanderer',
-      image: 'card04.jpg'
-    },
-    {
-      id: 5,
-      title: 'The Owl',
-      character: 'neutral',
-      image: 'card05.jpg'
-    },
-    {
-      id: 6,
-      title: 'Fontanna',
-      image: 'card06.jpg'
+  let deck = function() {
+    let deckData = [{
+        id: 0,
+        title: 'Start',
+        image: 'end.jpg'
+      },
+      {
+        id: 1,
+        title: 'The Paladin',
+        image: 'card01.jpg'
+      },
+      {
+        id: 2,
+        title: 'The Keep',
+        image: 'card02.png'
+      },
+      {
+        id: 3,
+        title: 'The Desert',
+        image: 'card03.jpg'
+      },
+      {
+        id: 4,
+        title: 'The Wanderer',
+        image: 'card04.jpg'
+      },
+      {
+        id: 5,
+        title: 'The Owl',
+        character: 'neutral',
+        image: 'card05.jpg'
+      },
+      {
+        id: 6,
+        title: 'Fontanna',
+        image: 'card06.jpg'
+      }
+    ];
+    let index = 0;
+
+    let nextCard = function() {
+      index++;
+      index = index === deckData.length ? 0 : index;
+      return deckData[index];
     }
-  ];
 
-  function schuffle(a) {
-    return a;
-  };
+    return {
+      nextCard: nextCard
+    }
+  }();
 
-  let currentIndex = 0;
   let nextButton = document.getElementById("nextCard");
   let title = document.getElementById("cardTitle");
   let image = document.getElementById("cardImage");
 
-  function nextCard() {
-    currentIndex++;
-    currentIndex = currentIndex === deck.length ? 0 : currentIndex;
-  };
+  let card = deck.nextCard;
 
-  function display() {
-    title.innerHTML = deck[currentIndex].title;
-    image.innerHTML = "<img src='img/" + deck[currentIndex].image + "'>";
+  function display(card) {
+    title.innerHTML = card.title;
+    image.innerHTML = "<img src='img/" + card.image + "'>";
   }
 
-  display();
+  display(card());
 
   nextButton.addEventListener('click', function() {
-    nextCard();
-    display();
+    display(card());
   });
 })();
